49f4c45881256ecb1895cf2a48caf5e1f6805fe3
iamsarvagya1296 <iamsarvagya.saxena@gmail.com>
2023-11-14 12:06:29 +0530

    store and fetch data from localstorage to check credentials in login and signup

---
+++ src/components/Login.jsx
@@ -0,0 +1,65 @@
+import React,{useEffect, useState} from 'react'
+
+const Login = () => {
+    
+    const [obj, setObj] = useState({email:"", password:""})
+    const [credentials, setCredentials] = useState({});
+
+
+    useEffect(() => {
+       
+        setCredentials(JSON.parse(localStorage.getItem("todo")))
+    
+      
+    }, [])
+    
+    
+
+    const handleChange = (e) =>{
+       setObj({...obj,[e.target.name]:e.target.value})
+    }
+    
+    const handleClick = (e) => {
+        e.preventDefault();
+        if(obj.email === credentials.email){
+        if(obj.password === credentials.password){
+         alert("Login Successfully")
+        }else{
+            alert("Wrong Credentials")
+         
+        }
+        }else{
+            alert("Wrong Credentials")
+        }
+
+
+
+    }
+    
+    return (
+    <div className='w-full h-screen flex items-center justify-center bg-blue-400'>
+    <div className='bg-white w-[400px] p-8'>
+    <h1 className='text-2xl font-semibold text-center mb-4'>Login</h1>
+
+    <form action="">
+     <label htmlFor="">email</label>
+     <input type="email" name="email" id="email" value= {obj.email} onChange={handleChange} className='mb-4 w-full border py-2 px-3 focus:outline-none focus:border-blue-400' />
+
+     <label htmlFor="password">password</label>
+     <input type="password" name="password" id="password" value = {obj.password} 
+     onChange={handleChange} className='mb-4 w-full border focus:outline-none py-2 px-3 focus:border-blue-400'  />
+
+     <button type="submit" onClick={handleClick} className='w-full bg-blue-500 text-white hover:bg-blue-600 py-2 px-4'>Login</button>
+     
+    
+    
+    </form>
+
+
+    </div>
+      
+    </div>
+  )
+}
+
+export default Login
---
+++ src/components/Signup.jsx
@@ -0,0 +1,48 @@
+import React,{useState} from 'react'
+
+const Signup = () => {
+    const [obj, setObj] = useState({name:"",email:"", password:""})
+
+    
+
+    const handleChange = (e) =>{
+       setObj({...obj,[e.target.name]:e.target.value})
+    }
+    
+    const handleClick = (e) => {
+        e.preventDefault();
+        localStorage.setItem("todo",JSON.stringify(obj))
+         }
+
+    
+    return (
+    <div className='w-full h-screen flex items-center justify-center bg-blue-400'>
+    <div className='bg-white w-[400px] p-8'>
+    <h1 className='text-2xl font-semibold text-center mb-4'>Signup</h1>
+
+    <form action="">
+
+    <label htmlFor="name">name</label>
+    <input type="text" name="name" id="name" value = {obj.name} onChange={handleChange} className='mb-4 w-full py-2 px-3 border focus:outline-none focus:border-blue-400' />
+
+     <label htmlFor="email">email</label>
+     <input type="email" name="email" id="email" value= {obj.email} onChange={handleChange} className='mb-4 w-full border py-2 px-3 focus:outline-none focus:border-blue-400' />
+
+     <label htmlFor="password">password </label>
+     <input type="password" name="password" id="password" value = {obj.password} 
+     onChange={handleChange} className='mb-4 w-full border focus:outline-none py-2 px-3 focus:border-blue-400'  />
+
+     <button type="submit" onClick={handleClick} className='w-full bg-blue-500 text-white hover:bg-blue-600 py-2 px-4'>Signup</button>
+     
+    
+    
+    </form>
+
+
+    </div>
+      
+    </div>
+  )
+}
+
+export default Signup
